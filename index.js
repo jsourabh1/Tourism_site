c7ebc7463490ed948cf2ee39240d1bdd5e0cf6db
Saurabh Jain <jsourabh861@gmail.com>
2022-01-24 00:12:44 +0530

    Update index.js
    1) Add the slider for the review section from mySwiper.com

--- index.js
+++ index.js
@@ -71,3 +71,22 @@ videobtn.forEach(btn => {
 
 
 // book section javascript
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
