42c8a45ad2bb1303e519de67daf27b77b7704a0b
Saurabh Jain <jsourabh861@gmail.com>
2022-01-24 01:52:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,20 +73,20 @@ videobtn.forEach(btn => {
 // book section javascript
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
-    loop: true,
+    loop:true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     breakpoints: {
         640: {
-            slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-            slidesPerView: 2,
+          slidesPerView: 2,
         },
         1024: {
-            slidesPerView: 3,
+          slidesPerView: 3,
         },
     },
 });
